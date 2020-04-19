e214ee475a1298fbc30ba0d3460ce2f2cb157f75
Lucio Nonato <luciononato@hotmail.com>
2020-04-19 09:41:39 -0300

    4 commit
    tipagem de dados. Uso de biblioteca date-fns  e suas funções.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,20 +1,40 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
 // salvando dados em array
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, reponse) => {
   // provider = profissional que ira atender o cliente
   // data de agendameto
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  // verifica se ja existe agendamento nessa data
+  if (findAppointmentInSameDate) {
+    return reponse
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
